crdroidandroid/android_packages_apps_crDroidSettings

Add custom seekbar preferences for lineage settings
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/crdroid/settings/preferences/LineageSecureSettingSeekBarPreference.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2016-2019 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.crdroid.settings.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class LineageSecureSettingSeekBarPreference extends CustomSeekBarPreference {
+
+    public LineageSecureSettingSeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new LineageSecureSettingsStore(context.getContentResolver()));
+    }
+
+    public LineageSecureSettingSeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new LineageSecureSettingsStore(context.getContentResolver()));
+    }
+
+    public LineageSecureSettingSeekBarPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new LineageSecureSettingsStore(context.getContentResolver()));
+    }
+}

src/com/crdroid/settings/preferences/LineageSecureSettingsStore.java
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2016-2019 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.crdroid.settings.preferences;
+
+import android.content.ContentResolver;
+import android.preference.PreferenceDataStore;
+import android.os.UserHandle;
+
+import lineageos.providers.LineageSettings;
+
+
+public class LineageSecureSettingsStore extends android.support.v7.preference.PreferenceDataStore
+        implements PreferenceDataStore {
+
+    private ContentResolver mContentResolver;
+
+    public LineageSecureSettingsStore(ContentResolver contentResolver) {
+        mContentResolver = contentResolver;
+    }
+
+    public boolean getBoolean(String key, boolean defValue) {
+        return LineageSettings.Secure.getIntForUser(mContentResolver, key, defValue ? 1 : 0, UserHandle.USER_CURRENT) != 0;
+    }
+
+    public float getFloat(String key, float defValue) {
+        return LineageSettings.Secure.getFloatForUser(mContentResolver, key, defValue, UserHandle.USER_CURRENT);
+    }
+
+    public int getInt(String key, int defValue) {
+        return LineageSettings.Secure.getIntForUser(mContentResolver, key, defValue, UserHandle.USER_CURRENT);
+    }
+
+    public long getLong(String key, long defValue) {
+        return LineageSettings.Secure.getLongForUser(mContentResolver, key, defValue, UserHandle.USER_CURRENT);
+    }
+
+    public String getString(String key, String defValue) {
+        String result = LineageSettings.Secure.getString(mContentResolver, key);
+        return result == null ? defValue : result;
+    }
+
+    public void putBoolean(String key, boolean value) {
+        putInt(key, value ? 1 : 0);
+    }
+
+    public void putFloat(String key, float value) {
+        LineageSettings.Secure.putFloatForUser(mContentResolver, key, value, UserHandle.USER_CURRENT);
+    }
+
+    public void putInt(String key, int value) {
+        LineageSettings.Secure.putIntForUser(mContentResolver, key, value, UserHandle.USER_CURRENT);
+    }
+
+    public void putLong(String key, long value) {
+        LineageSettings.Secure.putLongForUser(mContentResolver, key, value, UserHandle.USER_CURRENT);
+    }
+
+    public void putString(String key, String value) {
+        LineageSettings.Secure.putString(mContentResolver, key, value);
+    }
+}

src/com/crdroid/settings/preferences/LineageSystemSettingSeekBarPreference.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2016-2019 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.crdroid.settings.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class LineageSystemSettingSeekBarPreference extends CustomSeekBarPreference {
+
+    public LineageSystemSettingSeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new LineageSystemSettingsStore(context.getContentResolver()));
+    }
+
+    public LineageSystemSettingSeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new LineageSystemSettingsStore(context.getContentResolver()));
+    }
+
+    public LineageSystemSettingSeekBarPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new LineageSystemSettingsStore(context.getContentResolver()));
+    }
+}

src/com/crdroid/settings/preferences/LineageSystemSettingsStore.java
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2016-2019 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.crdroid.settings.preferences;
+
+import android.content.ContentResolver;
+import android.preference.PreferenceDataStore;
+import android.os.UserHandle;
+
+import lineageos.providers.LineageSettings;
+
+
+public class LineageSystemSettingsStore extends android.support.v7.preference.PreferenceDataStore
+        implements PreferenceDataStore {
+
+    private ContentResolver mContentResolver;
+
+    public LineageSystemSettingsStore(ContentResolver contentResolver) {
+        mContentResolver = contentResolver;
+    }
+
+    public boolean getBoolean(String key, boolean defValue) {
+        return LineageSettings.System.getIntForUser(mContentResolver, key, defValue ? 1 : 0, UserHandle.USER_CURRENT) != 0;
+    }
+
+    public float getFloat(String key, float defValue) {
+        return LineageSettings.System.getFloatForUser(mContentResolver, key, defValue, UserHandle.USER_CURRENT);
+    }
+
+    public int getInt(String key, int defValue) {
+        return LineageSettings.System.getIntForUser(mContentResolver, key, defValue, UserHandle.USER_CURRENT);
+    }
+
+    public long getLong(String key, long defValue) {
+        return LineageSettings.System.getLongForUser(mContentResolver, key, defValue, UserHandle.USER_CURRENT);
+    }
+
+    public String getString(String key, String defValue) {
+        String result = LineageSettings.System.getString(mContentResolver, key);
+        return result == null ? defValue : result;
+    }
+
+    public void putBoolean(String key, boolean value) {
+        putInt(key, value ? 1 : 0);
+    }
+
+    public void putFloat(String key, float value) {
+        LineageSettings.System.putFloatForUser(mContentResolver, key, value, UserHandle.USER_CURRENT);
+    }
+
+    public void putInt(String key, int value) {
+        LineageSettings.System.putIntForUser(mContentResolver, key, value, UserHandle.USER_CURRENT);
+    }
+
+    public void putLong(String key, long value) {
+        LineageSettings.System.putLongForUser(mContentResolver, key, value, UserHandle.USER_CURRENT);
+    }
+
+    public void putString(String key, String value) {
+        LineageSettings.System.putString(mContentResolver, key, value);
+    }
+}

BlissRoms/platform_vendor_support

Change-Id: I8ed8ad657111322a40482533566ead7b42332d9d

src/com/bliss/support/preferences/LineageSecureSettingSeekBarPreference.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2016-2019 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bliss.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class LineageSecureSettingSeekBarPreference extends CustomSeekBarPreference {
+
+    public LineageSecureSettingSeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new LineageSecureSettingsStore(context.getContentResolver()));
+    }
+
+    public LineageSecureSettingSeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new LineageSecureSettingsStore(context.getContentResolver()));
+    }
+
+    public LineageSecureSettingSeekBarPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new LineageSecureSettingsStore(context.getContentResolver()));
+    }
+}

src/com/bliss/support/preferences/LineageSecureSettingsStore.java
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2016-2019 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bliss.support.preferences;
+
+import android.content.ContentResolver;
+import android.preference.PreferenceDataStore;
+import android.os.UserHandle;
+
+import lineageos.providers.LineageSettings;
+
+
+public class LineageSecureSettingsStore extends androidx.preference.PreferenceDataStore
+        implements PreferenceDataStore {
+
+    private ContentResolver mContentResolver;
+
+    public LineageSecureSettingsStore(ContentResolver contentResolver) {
+        mContentResolver = contentResolver;
+    }
+
+    public boolean getBoolean(String key, boolean defValue) {
+        return LineageSettings.Secure.getIntForUser(mContentResolver, key, defValue ? 1 : 0, UserHandle.USER_CURRENT) != 0;
+    }
+
+    public float getFloat(String key, float defValue) {
+        return LineageSettings.Secure.getFloatForUser(mContentResolver, key, defValue, UserHandle.USER_CURRENT);
+    }
+
+    public int getInt(String key, int defValue) {
+        return LineageSettings.Secure.getIntForUser(mContentResolver, key, defValue, UserHandle.USER_CURRENT);
+    }
+
+    public long getLong(String key, long defValue) {
+        return LineageSettings.Secure.getLongForUser(mContentResolver, key, defValue, UserHandle.USER_CURRENT);
+    }
+
+    public String getString(String key, String defValue) {
+        String result = LineageSettings.Secure.getString(mContentResolver, key);
+        return result == null ? defValue : result;
+    }
+
+    public void putBoolean(String key, boolean value) {
+        putInt(key, value ? 1 : 0);
+    }
+
+    public void putFloat(String key, float value) {
+        LineageSettings.Secure.putFloatForUser(mContentResolver, key, value, UserHandle.USER_CURRENT);
+    }
+
+    public void putInt(String key, int value) {
+        LineageSettings.Secure.putIntForUser(mContentResolver, key, value, UserHandle.USER_CURRENT);
+    }
+
+    public void putLong(String key, long value) {
+        LineageSettings.Secure.putLongForUser(mContentResolver, key, value, UserHandle.USER_CURRENT);
+    }
+
+    public void putString(String key, String value) {
+        LineageSettings.Secure.putString(mContentResolver, key, value);
+    }
+}

src/com/bliss/support/preferences/LineageSystemSettingSeekBarPreference.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2016-2019 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bliss.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class LineageSystemSettingSeekBarPreference extends CustomSeekBarPreference {
+
+    public LineageSystemSettingSeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new LineageSystemSettingsStore(context.getContentResolver()));
+    }
+
+    public LineageSystemSettingSeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new LineageSystemSettingsStore(context.getContentResolver()));
+    }
+
+    public LineageSystemSettingSeekBarPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new LineageSystemSettingsStore(context.getContentResolver()));
+    }
+}

src/com/bliss/support/preferences/LineageSystemSettingsStore.java
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2016-2019 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bliss.support.preferences;
+
+import android.content.ContentResolver;
+import android.preference.PreferenceDataStore;
+import android.os.UserHandle;
+
+import lineageos.providers.LineageSettings;
+
+
+public class LineageSystemSettingsStore extends androidx.preference.PreferenceDataStore
+        implements PreferenceDataStore {
+
+    private ContentResolver mContentResolver;
+
+    public LineageSystemSettingsStore(ContentResolver contentResolver) {
+        mContentResolver = contentResolver;
+    }
+
+    public boolean getBoolean(String key, boolean defValue) {
+        return LineageSettings.System.getIntForUser(mContentResolver, key, defValue ? 1 : 0, UserHandle.USER_CURRENT) != 0;
+    }
+
+    public float getFloat(String key, float defValue) {
+        return LineageSettings.System.getFloatForUser(mContentResolver, key, defValue, UserHandle.USER_CURRENT);
+    }
+
+    public int getInt(String key, int defValue) {
+        return LineageSettings.System.getIntForUser(mContentResolver, key, defValue, UserHandle.USER_CURRENT);
+    }
+
+    public long getLong(String key, long defValue) {
+        return LineageSettings.System.getLongForUser(mContentResolver, key, defValue, UserHandle.USER_CURRENT);
+    }
+
+    public String getString(String key, String defValue) {
+        String result = LineageSettings.System.getString(mContentResolver, key);
+        return result == null ? defValue : result;
+    }
+
+    public void putBoolean(String key, boolean value) {
+        putInt(key, value ? 1 : 0);
+    }
+
+    public void putFloat(String key, float value) {
+        LineageSettings.System.putFloatForUser(mContentResolver, key, value, UserHandle.USER_CURRENT);
+    }
+
+    public void putInt(String key, int value) {
+        LineageSettings.System.putIntForUser(mContentResolver, key, value, UserHandle.USER_CURRENT);
+    }
+
+    public void putLong(String key, long value) {
+        LineageSettings.System.putLongForUser(mContentResolver, key, value, UserHandle.USER_CURRENT);
+    }
+
+    public void putString(String key, String value) {
+        LineageSettings.System.putString(mContentResolver, key, value);
+    }
+}